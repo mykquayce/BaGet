b42a2c50a55ef4f868ac7159f404a8ea4f3f96a7
bob h <mail@bob.codes>
2022-06-19 19:43:49 +0100

    nuget updates and a build script

---
+++ build.ps1
@@ -0,0 +1,11 @@
+docker pull mcr.microsoft.com/dotnet/aspnet:6.0
+if (!$?) { exit 1; }
+
+docker pull mcr.microsoft.com/dotnet/sdk:6.0
+if (!$?) { exit 1; }
+
+docker build --tag eassbhhtgu/baget:latest .
+if (!$?) { exit 1; }
+
+docker push eassbhhtgu/baget:latest
+if (!$?) { exit 1; }
