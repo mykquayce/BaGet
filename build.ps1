46e5d3634551200338d559c288620c142c4a35a7
bob h <mail@bob.codes>
2024-04-04 13:56:06 +0100

    work so far

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
-docker pull mcr.microsoft.com/dotnet/aspnet:7.0
+docker pull mcr.microsoft.com/dotnet/aspnet:8.0
 if (!$?) { exit 1; }
 
-docker pull mcr.microsoft.com/dotnet/sdk:7.0
+docker pull mcr.microsoft.com/dotnet/sdk:8.0
 if (!$?) { exit 1; }
 
 docker build --tag eassbhhtgu/baget:latest .
